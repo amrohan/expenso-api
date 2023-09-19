63a83dc671b048bab7fdcfb124e9f9ab783ea298
amrohan <amrohanx@gmail.com>
2023-09-19 13:29:36 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@
 
 // Read the .env file.
 import * as dotenv from "dotenv";
-import cors from "@fastify/cors";
-
 dotenv.config();
 
 // Require the framework
@@ -14,16 +12,8 @@ const app = Fastify({
   logger: true,
 });
 
-app.register(cors, {
-  // put your options here
-});
 // Register your application as a normal plugin.
-// app.register(cors, import("../src/index.js"));
-import("../src/index.js").then((module) => {
-  // Do something with the imported module
-  // For example, you can register it as a plugin
-  app.register(module.default);
-});
+app.register(import("../src/index.js"));
 
 export default async (req, res) => {
   await app.ready();

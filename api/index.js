2190a550499185fea671deeb9b9ac838e18d2e4a
amrohan <amrohanx@gmail.com>
2023-09-19 13:26:22 +0530

    tets

--- api/index.js
+++ api/index.js
@@ -14,8 +14,16 @@ const app = Fastify({
   logger: true,
 });
 
+app.register(cors, {
+  // put your options here
+});
 // Register your application as a normal plugin.
-app.register(cors, import("../src/index.js"));
+// app.register(cors, import("../src/index.js"));
+import("../src/index.js").then((module) => {
+  // Do something with the imported module
+  // For example, you can register it as a plugin
+  app.register(module.default);
+});
 
 export default async (req, res) => {
   await app.ready();

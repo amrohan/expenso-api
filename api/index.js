8525091b60090b3716428088af2a365c0c97ccdd
amrohan <amrohanx@gmail.com>
2023-09-20 15:30:12 +0530

    added cors

--- api/index.js
+++ api/index.js
@@ -1,3 +1,25 @@
+// "use strict";
+
+// // Read the .env file.
+// import * as dotenv from "dotenv";
+// dotenv.config();
+
+// // Require the framework
+// import Fastify from "fastify";
+
+// // Instantiate Fastify with some config
+// const app = Fastify({
+//   logger: true,
+// });
+
+// // Register your application as a normal plugin.
+// app.register(import("../src/index.js"));
+
+// export default async (req, res) => {
+//   await app.ready();
+//   app.server.emit("request", req, res);
+// };
+
 "use strict";
 
 // Read the .env file.
@@ -7,9 +29,19 @@ dotenv.config();
 // Require the framework
 import Fastify from "fastify";
 
+// Import the cors module
+import cors from "@fastify/cors";
+
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
+});
+
+// Register the CORS plugin
+app.register(cors, {
+  origin: ["http://localhost:3000", "https://piggysync-api.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 });
 
 // Register your application as a normal plugin.
